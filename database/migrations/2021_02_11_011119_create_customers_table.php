a87e20bdb97e78ada2a8348b5e9ecb1a96d34e5d
Fernando Ferreira <fernandoferreira67@hotmail.com>
2021-02-10 22:41:41 -0300

    Created Migration Customers

---
+++ database/migrations/2021_02_11_011119_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fullname');
+            $table->string('cpf')->unique();
+            $table->string('rg');
+            $table->string('address');
+            $table->string('district');
+            $table->string('cep');
+            $table->string('city');
+            $table->string('phone')->nullable();
+            $table->string('cellphone')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
